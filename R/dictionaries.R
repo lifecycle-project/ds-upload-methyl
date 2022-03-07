aaf8a7f6a4fb5d2b3142c38556072a09eefe4d1e
Sido Haakma <sido@haakma.org>
2022-03-07 11:49:04 +0100

    fix: table names

--- R/dictionaries.R
+++ R/dictionaries.R
@@ -72,12 +72,9 @@ du.dict.retrieve.tables <- function(api_url, dict_name, dict_version, data_versi
   tables <- dictionaries %>%
     select("name") %>%
     pmap(function(name) {
-      if (!beta) {
-        canonical_table_name <- strsplit(name, "_")
-        table <- paste0(data_version, "_", canonical_table_name[1][3])
-      } else {
-        table <- paste0(data_version, "_", path_ext_remove(name))
-      }
+      canonical_table_name <- unlist(strsplit(name, "_"))
+      table_name_with_ext <- canonical_table_name[3]
+      table <- paste0(data_version, "_", path_ext_remove(table_name_with_ext))
       return(data.frame(table = table, file_name = name))
     })
 
